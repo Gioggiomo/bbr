3a93a78995b31c60b41dc6652286f2e379f6b8eb
Gioggiomo <ohm_92@hotmail.it>
2019-07-16 13:14:58 +0200

    default of router-1

--- router-2.sh
+++ router-2.sh
@@ -6,18 +6,23 @@ add-apt-repository add-apt-repository "deb [arch=amd64] https://apps3.cumulusnet
 apt-get update
 apt-get install -y frr --assume-yes --force-yes
 
+
 # Forwarding enabled
 sysctl -w net.ipv4.ip_forward=1
 
-# Setting up interface eth1 (South)
+# setting BBR as TCP congestion control
+sysctl -w net.core.default_qdisc=fq
+sysctl -w net.ipv4.tcp_congestion_control=bbr
+
+# Setting up interface eth1 (North)
 ip link set dev eth1 up
 ip addr add 172.23.1.33/30 dev eth1
 
-# Setting up interface eth2 (West)
+# Setting up interface eth2 (South)
 ip link set dev eth2 up
 ip addr add 172.23.1.38/30 dev eth2
 
-# Setting up interface eth3 (South)
+# Setting up interface eth3 (North)
 ip link set dev eth3 up
 ip addr add 172.23.1.41/30 dev eth3
 
