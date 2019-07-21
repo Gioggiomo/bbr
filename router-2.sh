1d5332e3ef55f8783999592815d13d77af06ff61
Gioggiomo <ohm_92@hotmail.it>
2019-07-21 02:02:16 +0200

    default router of router-1

--- router-2.sh
+++ router-2.sh
@@ -14,18 +14,47 @@ sysctl -w net.ipv4.ip_forward=1
 sysctl -w net.core.default_qdisc=fq
 sysctl -w net.ipv4.tcp_congestion_control=bbr
 
+echo "
+
+#################################################
+#                                               #
+#                                               #
+#          TEST ON BBR AUTOMATICALLY SET        #
+#                                               #
+#                                               #
+#             IF YOU SEE AS BELOW               #
+#                                               #
+#      net.ipv4.tcp_congestion_control=bbr      #
+#                                               #
+#          EVERYTHING HAS WORKED FINE           #
+#                                               #
+#                                               #
+#                                               #
+#################################################
+
+
+"
+
+# Checking
+sysctl -p
+sysctl net.ipv4.tcp_congestion_control
+
+# Enable packet pacing to "NGbps"
+# sbin/tc qdisc add dev eth1 root fq maxrate Ngbit
+
+
 # Setting up interface eth1 (North)
 ip link set dev eth1 up
-ip addr add 172.23.1.33/30 dev eth1
+ip addr add 172.23.3.1/24 dev eth1
 
-# Setting up interface eth2 (South)
-ip link set dev eth2 up
-ip addr add 172.23.1.38/30 dev eth2
-
-# Setting up interface eth3 (North)
+# Setting up interface eth3 (South)
 ip link set dev eth3 up
-ip addr add 172.23.1.41/30 dev eth3
+ip addr add 172.23.2.2/30 dev eth3
+
+# Setting up interface eth2 (North)
+ip link set dev eth2 up
+ip addr add 172.23.4.1/24 dev eth2
 
 # set router-1 ad default router
 ip route del default
-ip route add default via 172.23.1.37
+ip route add default via 172.23.2.1
